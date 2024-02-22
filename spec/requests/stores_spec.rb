b029ad166d16fcc167b2b7d96b8d68c2f9bff3b7
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-02-22 19:10:49 -0300

    feat: inicia implementação de testes bundle exec rspec

--- spec/requests/stores_spec.rb
+++ spec/requests/stores_spec.rb
@@ -18,11 +18,11 @@ RSpec.describe "/stores", type: :request do
   # Store. As you add validations to Store, be sure to
   # adjust the attributes here as well.
   let(:valid_attributes) {
-    skip("Add a hash of attributes valid for your model")
+    { name: "Great restaurant"}
   }
 
   let(:invalid_attributes) {
-    skip("Add a hash of attributes invalid for your model")
+    {name: ""}
   }
 
   describe "GET /index" do
