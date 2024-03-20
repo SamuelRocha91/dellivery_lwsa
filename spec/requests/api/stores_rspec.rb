c9bb0b95106540bdba95011b0d494e9fde389d69
SamuelRocha91 <samuel_sr@hotmail.com.br>
2024-03-20 11:42:20 -0300

    feat: implementa refatoração de testes

--- spec/requests/api/stores_rspec.rb
+++ spec/requests/api/stores_rspec.rb
@@ -3,7 +3,9 @@ require 'rails_helper'
 RSpec.describe "/stores", type: :request do
    describe "GET /show" do
      it "render a sucessful response with stores data" do
-       store = Store.create! name: "Greatest store"
+       user = User.create! email: "sam@gmail.com", password: "kosjksjd123235@", password_confirmation:"kosjksjd123235@"
+       store = Store.create! name: "Greatest store", user: user
+       sign_in(user)
        get "/stores/#{store.id}", headers: {"Accept" => "application/json"}
        json = JSON.parse(response.body)
 
